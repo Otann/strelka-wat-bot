cff45115cea5f85b5bef53e17f5df5ec1193b87b
Anton Chebotaev <anton.chebotaev@gmail.com>
2016-02-22 00:49:43 +0300

    Landing page

--- src/strelka_wat_bot/handler.clj
+++ src/strelka_wat_bot/handler.clj
@@ -1,10 +1,12 @@
 (ns strelka-wat-bot.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+
+            [strelka-wat-bot.static :as static]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] static/home)
   (route/not-found "Not Found"))
 
 (def app
---
+++ src/strelka_wat_bot/static.clj
@@ -0,0 +1,28 @@
+(ns strelka-wat-bot.static
+  "Layouts for server-side rendered pages"
+  (:require [hiccup.core :refer [html]]
+            [hiccup.page :refer [include-js include-css]]))
+
+(def home
+  (html
+    [:html
+     [:head
+      [:meta {:charset "utf-8"}]
+      [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+      [:title "Strelka Wat Bot"]
+
+      (include-css "//cdn.jsdelivr.net/octicons/3.3.0/octicons.css")
+      (include-css "//oss.maxcdn.com/semantic-ui/2.1.7/semantic.min.css")]
+
+     [:body
+      [:div.ui.container
+       [:div.ui.vertical.masthead.center.aligned.segment
+        {:style "padding-top: 100px"}
+        [:div.ui.test.container
+         [:h1.header {:style {:margin-top "2em"
+                              :font-size "3em"}}
+          "Welcome to Strelka Wat Bot"]
+         [:h3 "Telegram bot that knows what's happening in Strelka Institute"]
+         [:a.ui.huge.primary.button {:href "https://telegram.me/StrelkaWatBot"}
+          "Start Conversation"
+          [:i.icon.right.arrow]]]]]]]))
